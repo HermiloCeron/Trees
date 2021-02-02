2a220a12ca1767bbe2ea7df5180d0353158133ad
tomg <goresthomasj@johndeere.com>
2021-02-02 16:22:45 -0600

    Add data in trees.js  model

---
+++ models/soilType.js
@@ -0,0 +1,49 @@
+// Soil types https://arbordayblog.org/treecare/dish-dirt-soil-matters-tree-health/
+[{
+    id : 1,
+    name : 'Sandy',
+    moisture : 'low',
+    nutrientContent: 'low',
+    soilDepth: 'low'
+},
+
+{
+    id : 2,
+    name : 'Silt',
+    moisture : 'high',
+    nutrientContent: "high",
+    soilDepth: 'medium'
+},
+
+{
+    id : 3,
+    name : 'Clay',
+    moisture : 'low',
+    nutrientContent: "low",
+    soilDepth: "low"
+},
+
+{
+    id : 4,
+    name : 'Peaty',
+    moisture : 'high',
+    nutrientContent: "low",
+    soilDepth: "low"
+},
+
+{
+    id : 5,
+    name : 'Loamy',
+    moisture : 'high',
+    nutrientContent: "high",
+    soilDepth: "high"
+},
+
+{
+    id : 6,
+    name : 'Chalky',
+    moisture : 'low',
+    nutrientContent: "low",
+    soilDepth: "low"
+},
+]
