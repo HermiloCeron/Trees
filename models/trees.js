085a8553396a70ccc3706c4aa45523d3e9d5e402
tomg <goresthomasj@johndeere.com>
2021-02-02 09:08:32 -0600

    moved data into a separate trees.js file, created index.ejs, new.ejs, routes in server.js

---
+++ models/trees.js
@@ -0,0 +1,26 @@
+let trees=
+[
+    // Red pine https://www.ontario.ca/page/red-pine
+    {
+        id : 1,
+        name : "Red pine",
+        img : "https://es.wikipedia.org/wiki/Pinus_resinosa#/media/Archivo:Pinus_resinosa.jpg",
+        height: "Large"
+    },
+    // Periuvian pepper
+    {
+        id : 2,
+        name : "Peruvian pepper",
+        img : "https://es.wikipedia.org/wiki/Schinus_molle#/media/Archivo:Schinus_molle01.jpg",
+        height: "Medium"
+    },
+    // Japanesse maple
+    {
+        id : 3,
+        name : "Japanese Maple",
+        img : "https://en.wikipedia.org/wiki/Acer_palmatum#/media/File:Acer_palmatum_BotGartenMuenster_Faecherahorn_6691.jpg",
+        height: "Small"
+    },
+]
+
+module.exports = trees;
--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+const trees = require('./models/trees.js');
+
 const express = require('express');
 const app = express();//app is an object
 
-app.get('/somedata', (req, res) => {
-    res.send('here is your information');
+// middleware
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/trees/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+app.get('/trees', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/trees/:id', (req, res) => {
+    res.render('show.ejs', { //second param must be an object
+        tree: trees[req.params.id] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.index]
+    });
+});
+
+app.post('/trees', (req, res) => {
+    trees.push(req.body);
+    res.redirect('/trees'); //send the user back to /trees
 });
 
 app.listen(3000, ()=>{
