08da9a5900a6c184723677bf7036883a3ed64910
tomg <goresthomasj@johndeere.com>
2021-02-02 13:54:22 -0600

    Add Users routes, controllers, views

--- controllers/users.js
+++ controllers/users.js
@@ -12,8 +12,38 @@ const login = (req, res) => {
     res.render('./users/login.ejs')
 }
 
+const showProfile = (req, res) => {
+    res.render('./users/profile.ejs', {
+        user:users[req.params.id],
+        id:req.params.id
+    })
+}
+
+const createUser = (req, res) => {
+    users.push(req.body)
+    res.redirect('/users/profile/'+ (users.length-1))
+}
+
+const editUser = (req, res) => {
+    users[req.params.id] = req.body
+    res.redirect('/users/profile/' + req.params.id)
+    }
+
+const loginUser = (req, res) => {
+    for (i=0; i<users.length; i++) {
+        if (users[i].username === req.body.username && users[i].password === req.body.password) {
+            res.redirect('/users/profile/' + i)        
+        }
+    }
+    res.redirect('/users/signup/')
+    }    
+
 module.exports = {
     renderHomepage,
     signup,
     login,
+    createUser,
+    showProfile,
+    editUser,
+    loginUser,
 }
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,10 @@ const ctrl = require('../controllers');
 router.get("/", ctrl.users.renderHomepage);
 router.get("/signup", ctrl.users.signup);
 router.get("/login", ctrl.users.login);
+router.get("/profile/:id", ctrl.users.showProfile);
+router.post("/profile", ctrl.users.createUser);
+router.post("/", ctrl.users.loginUser);
+router.put("/profile/:id", ctrl.users.editUser);
+
 
 module.exports = router;
